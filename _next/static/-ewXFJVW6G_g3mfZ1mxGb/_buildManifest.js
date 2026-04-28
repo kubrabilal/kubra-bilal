9a737ef706db38634b9945b4c69408821604648e
kubrabilal <kubrabilal@users.noreply.github.com>
2026-04-28 10:04:06 +0000

    deploy: f45bea3b3577dd1e4da33de65976e723e60718ae

---
+++ _next/static/-ewXFJVW6G_g3mfZ1mxGb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/92011b3a214337f7.css","static/chunks/pages/index-76c36b9e92b9f502.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
